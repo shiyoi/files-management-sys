93d653eb942f14ab95abf75142383a87723f805c
haozigege <liuhao713@aliyun.com>
2018-06-09 11:28:48 +0800

    集团档案-档案列表

--- src/views/new-components/contract-file-list/contract-file-list.vue
+++ src/views/new-components/contract-file-list/contract-file-list.vue
@@ -20,10 +20,14 @@
                       <TabPane label="简要信息">标签一的内容</TabPane>
                       <TabPane label="操作日志">
                           <div class="tab-info-con">
-                            <Table border :columns="columns2" :data="data2"></Table>
+                            <Table height="200" border :columns="columns2" :data="data2"></Table>
                           </div>
                       </TabPane>
-                      <TabPane label="借阅信息">标签三的内容</TabPane>
+                      <TabPane label="借阅信息">
+                          <div class="tab-info-con">
+                            <Table height="200" border :columns="columns3" :data="data3"></Table>
+                          </div>                          
+                      </TabPane>
                   </Tabs>
               </Col>
         </Row>
@@ -37,153 +41,241 @@ export default {
   name: 'contract-file',
   data: function () {
     return {
-       columns4: [
-                    {
-                        type: 'selection',
-                        width: 60,
-                        align: 'center'
-                    },
-                    {
-                        title: '合同编号',
-                        key: 'contract_number'
-                    },
-                    {
-                        title: '合同名称',
-                        key: 'contract_name'
-                    },
-                    {
-                        title: '腾邦签署主体',
-                        key: 'tempus_signing_subject'
-                    },
-                    {
-                        title: '档案归属',
-                        key: 'file_attribution'
-                    },
-                    {
-                        title: '对方公司名称',
-                        key: 'partner_company_name'
-                    },
-                    {
-                        title: '业务内容摘要',
-                        key: 'summary_of_content'
-                    },
-                    {
-                        title: '有效期',
-                        key: 'validity_period'
-                    },
-                    {
-                        title: '类别',
-                        key: 'category'
-                    },
-                    {
-                        title: '状态',
-                        key: 'status'
-                    },
-                    {
-                        title: '收文时间',
-                        key: 'receipt_time'
-                    },
-                    {
-                        title: '借阅情况',
-                        key: 'borrowing_situation'
-                    }                                                                                                                                                                                
-
-                ],
-      data1: [
-          {
-              contract_number: 'AHU889001231',
-              contract_name: 'xxxxx公司xxxx业务合同',
-              tempus_signing_subject: 'xxxxx单位名称',
-              file_attribution: '腾邦国际',
-              partner_company_name: 'xxxxxx公司',
-              summary_of_content:'这是内容摘要这是内...',
-              validity_period:'20180401-20190401',
-              category:'电子扫描件',
-              status:'已收文',
-              receipt_time:'20180401',
-              borrowing_situation:'超时未还'
-          },
-          {
-              contract_number: 'AHU889001231',
-              contract_name: 'xxxxx公司xxxx业务合同',
-              tempus_signing_subject: 'xxxxx单位名称',
-              file_attribution: '腾邦国际',
-              partner_company_name: 'xxxxxx公司',
-              summary_of_content:'这是内容摘要这是内...',
-              validity_period:'20180401-20190401',
-              category:'电子扫描件',
-              status:'已收文',
-              receipt_time:'20180401',
-              borrowing_situation:'超时未还'
-          },
-          {
-              contract_number: 'AHU889001231',
-              contract_name: 'xxxxx公司xxxx业务合同',
-              tempus_signing_subject: 'xxxxx单位名称',
-              file_attribution: '腾邦国际',
-              partner_company_name: 'xxxxxx公司',
-              summary_of_content:'这是内容摘要这是内...',
-              validity_period:'20180401-20190401',
-              category:'电子扫描件',
-              status:'已收文',
-              receipt_time:'20180401',
-              borrowing_situation:'超时未还'
-          },
-          {
-              contract_number: 'AHU889001231',
-              contract_name: 'xxxxx公司xxxx业务合同',
-              tempus_signing_subject: 'xxxxx单位名称',
-              file_attribution: '腾邦国际',
-              partner_company_name: 'xxxxxx公司',
-              summary_of_content:'这是内容摘要这是内...',
-              validity_period:'20180401-20190401',
-              category:'电子扫描件',
-              status:'已收文',
-              receipt_time:'20180401',
-              borrowing_situation:'超时未还'
-          }                              
-        
-      ],
-        columns2: [
-            {
-                title: 'Name',
-                key: 'name'
-            },
-            {
-                title: 'Age',
-                key: 'age'
-            },
-            {
-                title: 'Address',
-                key: 'address'
-            }
-        ],
-        data2: [
+    columns4: [
         {
-            name: 'John Brown',
-            age: 18,
-            address: 'New York No. 1 Lake Park',
-            date: '2016-10-03'
+            type: 'selection',
+            width: 60,
+            align: 'center'
         },
         {
-            name: 'Jim Green',
-            age: 24,
-            address: 'London No. 1 Lake Park',
-            date: '2016-10-01'
+            title: '合同编号',
+            key: 'contract_number'
         },
         {
-            name: 'Joe Black',
-            age: 30,
-            address: 'Sydney No. 1 Lake Park',
-            date: '2016-10-02'
+            title: '合同名称',
+            key: 'contract_name'
         },
         {
-            name: 'Jon Snow',
-            age: 26,
-            address: 'Ottawa No. 2 Lake Park',
-            date: '2016-10-04'
+            title: '腾邦签署主体',
+            key: 'tempus_signing_subject'
+        },
+        {
+            title: '档案归属',
+            key: 'file_attribution'
+        },
+        {
+            title: '对方公司名称',
+            key: 'partner_company_name'
+        },
+        {
+            title: '业务内容摘要',
+            key: 'summary_of_content'
+        },
+        {
+            title: '有效期',
+            key: 'validity_period'
+        },
+        {
+            title: '类别',
+            key: 'category'
+        },
+        {
+            title: '状态',
+            key: 'status'
+        },
+        {
+            title: '收文时间',
+            key: 'receipt_time'
+        },
+        {
+            title: '借阅情况',
+            key: 'borrowing_situation'
+        }              
+    ],
+    data1: [
+        {
+            contract_number: 'AHU889001231',
+            contract_name: 'xxxxx公司xxxx业务合同',
+            tempus_signing_subject: 'xxxxx单位名称',
+            file_attribution: '腾邦国际',
+            partner_company_name: 'xxxxxx公司',
+            summary_of_content:'这是内容摘要这是内...',
+            validity_period:'20180401-20190401',
+            category:'电子扫描件',
+            status:'已收文',
+            receipt_time:'20180401',
+            borrowing_situation:'超时未还'
+        },
+        {
+            contract_number: 'AHU889001231',
+            contract_name: 'xxxxx公司xxxx业务合同',
+            tempus_signing_subject: 'xxxxx单位名称',
+            file_attribution: '腾邦国际',
+            partner_company_name: 'xxxxxx公司',
+            summary_of_content:'这是内容摘要这是内...',
+            validity_period:'20180401-20190401',
+            category:'电子扫描件',
+            status:'已收文',
+            receipt_time:'20180401',
+            borrowing_situation:'超时未还'
+        },
+        {
+            contract_number: 'AHU889001231',
+            contract_name: 'xxxxx公司xxxx业务合同',
+            tempus_signing_subject: 'xxxxx单位名称',
+            file_attribution: '腾邦国际',
+            partner_company_name: 'xxxxxx公司',
+            summary_of_content:'这是内容摘要这是内...',
+            validity_period:'20180401-20190401',
+            category:'电子扫描件',
+            status:'已收文',
+            receipt_time:'20180401',
+            borrowing_situation:'超时未还'
+        },
+        {
+            contract_number: 'AHU889001231',
+            contract_name: 'xxxxx公司xxxx业务合同',
+            tempus_signing_subject: 'xxxxx单位名称',
+            file_attribution: '腾邦国际',
+            partner_company_name: 'xxxxxx公司',
+            summary_of_content:'这是内容摘要这是内...',
+            validity_period:'20180401-20190401',
+            category:'电子扫描件',
+            status:'已收文',
+            receipt_time:'20180401',
+            borrowing_situation:'超时未还'
+        }                              
+    
+    ],
+    columns2: [
+        {
+            title: '操作次数',
+            key: 'operating_frequency'
+        },
+        {
+            title: '操作人',
+            key: 'operating_people'
+        },
+        {
+            title: '类型',
+            key: 'type'
+        },
+        {
+            title: '时间',
+            key: 'time'
+        },
+        {
+            title: '操作',
+            key: 'operating'
+        }                        
+    ],
+    data2: [
+        {
+            operating_frequency: '5',
+            operating_people: '张三',
+            type: '修改',
+            time: '2018-05-06',
+            operating: `<span style="color:#029ddb;">查看操作日志</span>`
+        },
+        {
+            operating_frequency: '5',
+            operating_people: '张三',
+            type: '修改',
+            time: '2018-05-06',
+            operating: '查看操作日志'
+        },
+        {
+            operating_frequency: '5',
+            operating_people: '张三',
+            type: '修改',
+            time: '2018-05-06',
+            operating: '查看操作日志'
+        },
+        {
+            operating_frequency: '5',
+            operating_people: '张三',
+            type: '修改',
+            time: '2018-05-06',
+            operating: '查看操作日志'
         }
-    ]      
+    ],
+    columns3: [
+        {
+            title: '借出人',
+            key: 'lend_people'
+        },
+        {
+            title: '借阅人',
+            key: 'borrowed_people'
+        },
+        {
+            title: '借出时间',
+            key: 'lend_time'
+        },
+        {
+            title: '预计归还时间',
+            key: 'expected_return_time'
+        },
+        {
+            title: '实际归还时间',
+            key: 'actual_return_time'
+        },
+        {
+            title: '续借',
+            key: 'renew'
+        },
+        {
+            title: '操作',
+            key: 'operating'
+        }                                 
+    ],
+    data3: [
+        {
+            lend_people: '王子扬',
+            borrowed_people: '张三',
+            lend_time: '2018-05-06',
+            expected_return_time: '2018-05-06',
+            actual_return_time: '2018-05-06',
+            renew:'2018-05-06',
+            operating: ''
+        },
+        {
+            lend_people: '王子扬',
+            borrowed_people: '张三',
+            lend_time: '2018-05-06',
+            expected_return_time: '2018-05-06',
+            actual_return_time: '2018-05-06',
+            renew:'2018-05-06',
+            operating: ''
+        },
+        {
+            lend_people: '王子扬',
+            borrowed_people: '张三',
+            lend_time: '2018-05-06',
+            expected_return_time: '2018-05-06',
+            actual_return_time: '2018-05-06',
+            renew:'2018-05-06',
+            operating: ''
+        },
+        {
+            lend_people: '王子扬',
+            borrowed_people: '张三',
+            lend_time: '2018-05-06',
+            expected_return_time: '2018-05-06',
+            actual_return_time: '2018-05-06',
+            renew:'2018-05-06',
+            operating: ''
+        },
+        {
+            lend_people: '王子扬',
+            borrowed_people: '张三',
+            lend_time: '2018-05-06',
+            expected_return_time: '2018-05-06',
+            actual_return_time: '2018-05-06',
+            renew:'2018-05-06',
+            operating: ''
+        }                        
+    ]            
     };
   },
   components: {
