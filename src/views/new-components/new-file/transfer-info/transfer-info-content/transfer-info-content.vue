15e2592a30a3bf9f94f6ce9e5c44ab7c93ed860b
haozigege <liuhao713@aliyun.com>
2018-06-13 10:51:13 +0800

    6.13-提取新建档案里面表单的样式到公共样式main.less

---
+++ src/views/new-components/new-file/transfer-info/transfer-info-content/transfer-info-content.vue
@@ -0,0 +1,26 @@
+<style scoped lang="less">
+@import './transfer-info-content.less';
+</style>
+
+<template>
+  <div class="basic-info-con">
+               dvfvdsf              
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+   
+
+    };
+  },
+  components: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
--- src/views/new-components/new-file/transfer-info/transfer-info.vue
+++ src/views/new-components/new-file/transfer-info/transfer-info.vue
@@ -2,21 +2,23 @@
 @import './transfer-info.less';
 </style>
 <template>
-
     <div class="basicInfo">
-      <file-header text="合同档案基本信息"></file-header>
+      <file-header text="移交信息"></file-header>
+      <transfer-info-content></transfer-info-content>
     </div>        
 
 
 </template>
 <script>
 import fileHeader from '../file-basic-info/file-header/file-header.vue';
+import transferInfoContent from './transfer-info-content/transfer-info-content.vue';
 export default {
   data: function () {
     return {};
   },
   components: {
-    fileHeader
+    fileHeader,
+    transferInfoContent
   },
   methods: {
 
