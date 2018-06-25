f0b0848e370dca16aca5f1bd85c4582f119f8d99
haozigege <liuhao713@aliyun.com>
2018-06-25 09:15:39 +0800

    列表页面头部操作弹窗的数据分离

--- src/views/new-components/small-components/file-lists-top/file-lists-top.vue
+++ src/views/new-components/small-components/file-lists-top/file-lists-top.vue
@@ -4,27 +4,27 @@
 <template>
   <div class="list-top">
     <ul>
-      <li @click="search = !search;"><Icon size="14" type="search"></Icon><span>搜索</span></li>
+      <li @click="searchFields.search = !searchFields.search;"><Icon size="14" type="search"></Icon><span>搜索</span></li>
       <li @click="openNewFiles"><Icon size="14" type="plus"></Icon><span>新建</span></li>
       <li @click="batchImportFields.importBatch = !batchImportFields.importBatch;" style="width:90px;"><Icon size="14" type="reply-all"></Icon><span>批量导入</span></li>
-      <li @click="exportExcel = !exportExcel;"><Icon size="14" type="share"></Icon><span>导出</span></li>
+      <li @click="exportsExcel.exportExcel = !exportsExcel.exportExcel;"><Icon size="14" type="share"></Icon><span>导出</span></li>
       <li><Icon size="14" type="android-sync"></Icon><span>刷新</span></li>
       <li style="width:110px;"><Icon size="14" type="forward"></Icon><span>移入批量操作</span></li>
       <li><Icon size="14" type="android-print"></Icon><span>打印</span></li>
-      <li @click="migrate = !migrate;"><Icon size="14" type="social-dropbox"></Icon><span>迁移</span></li>
+      <li @click="migrateFields.migrate = !migrateFields.migrate;"><Icon size="14" type="social-dropbox"></Icon><span>迁移</span></li>
     </ul>
     <!-- 搜索弹窗 -->
-    <Modal v-model="search" :title="searchBoxTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
+    <Modal v-model="searchFields.search" :title="searchFields.searchBoxTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
       <div class="searchBox">
         <div class="rows">
           <div class="l">
             <div class="txt">腾邦签署主体</div>
-            <div class="in"><Input placeholder="" v-model="info.signedSubject" style="width:190px"></Input></div>
+            <div class="in"><Input placeholder="" v-model="searchFields.signedSubject" style="width:190px"></Input></div>
           </div>
           <div class="r">
             <div class="txt">档案归属</div>
             <div class="in">
-              <Select v-model="info.groupCompany" style="width:190px">
+              <Select v-model="searchFields.groupCompany" style="width:190px">
                 <Option v-for="item in fileAttribution" :value="item.value" :key="item.value">{{ item.label }}</Option>
               </Select>
             </div>
@@ -34,12 +34,12 @@
         <div class="rows">
           <div class="l">
             <div class="txt">业务内容摘要</div>
-            <div class="in"><Input placeholder="" v-model="info.businessBrief" style="width:190px"></Input></div>
+            <div class="in"><Input placeholder="" v-model="searchFields.businessBrief" style="width:190px"></Input></div>
           </div>
           <div class="r">
             <div class="txt">类别</div>
             <div class="in">
-              <Select v-model="info.contractType" style="width:190px">
+              <Select v-model="searchFields.contractType" style="width:190px">
                 <Option v-for="item in fileType" :value="item.value" :key="item.value">{{ item.label }}</Option>
               </Select>                
             </div>
@@ -49,12 +49,12 @@
         <div class="rows">
           <div class="l">
             <div class="txt">合同编号</div>
-            <div class="in"><Input placeholder="" v-model="info.contractNo" style="width:190px"></Input></div>
+            <div class="in"><Input placeholder="" v-model="searchFields.contractNo" style="width:190px"></Input></div>
           </div>
           <div class="r">
             <div class="txt">有效期</div>
             <div class="in">
-              <DatePicker type="daterange" v-model="info.effectiveDate" placement="bottom-end" placeholder="Select date" style="width: 190px"></DatePicker>
+              <DatePicker type="daterange" v-model="searchFields.effectiveDate" placement="bottom-end" placeholder="Select date" style="width: 190px"></DatePicker>
             </div>
           </div>
         </div>  
@@ -69,7 +69,7 @@
           <div class="r">
             <div class="txt">状态</div>
             <div class="in">
-              <Select v-model="info.status" style="width:190px">
+              <Select v-model="searchFields.status" style="width:190px">
                 <Option v-for="item in fileStatus" :value="item.value" :key="item.value">{{ item.label }}</Option>
               </Select>                 
             </div>
@@ -79,7 +79,7 @@
         <div class="rows">
           <div class="l">
             <div class="txt">对方公司名称</div>
-            <div class="in"><Input placeholder="" v-model="info.oppositeCompany" style="width:190px"></Input></div>
+            <div class="in"><Input placeholder="" v-model="searchFields.oppositeCompany" style="width:190px"></Input></div>
           </div>
           <div class="r">
             <div class="txt">借阅情况</div>
@@ -98,24 +98,24 @@
       </div>
     </Modal>
     <!-- 批量导出弹窗 -->
-    <Modal v-model="exportExcel" :title="exportBoxTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
+    <Modal v-model="exportsExcel.exportExcel" :title="exportsExcel.exportBoxTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
       <div class="searchBox">
         <div>您确定要将选定项导出Excel表格吗</div>     
       </div>
 
       <div slot="footer">
-        <Button @click="exportExcel = false;exportExcelSuccess = true" type="primary">确定</Button>
-        <Button @click="exportExcel = false">取消</Button>
+        <Button @click="exportsExcel.exportExcel = false;exportsExcel.exportExcelSuccess = true" type="primary">确定</Button>
+        <Button @click="exportsExcel.exportExcel = false">取消</Button>
       </div>
     </Modal>   
     <!-- 批量导出成功弹窗 -->
-    <Modal v-model="exportExcelSuccess" :title="exportBoxSuccessTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
+    <Modal v-model="exportsExcel.exportExcelSuccess" :title="exportsExcel.exportBoxSuccessTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
       <div class="searchBox">
         <div>导出成功</div>     
       </div>
 
       <div slot="footer">
-        <Button @click="exportExcelSuccess = false">完成</Button>
+        <Button @click="exportsExcel.exportExcelSuccess = false">完成</Button>
       </div>
     </Modal>   
     <!-- 批量导入弹窗  -->
@@ -135,7 +135,7 @@
       </div>
     </Modal> 
     <!-- 迁移弹窗   -->
-    <Modal v-model="migrate" :title="migrateTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
+    <Modal v-model="migrateFields.migrate" :title="migrateFields.migrateTitle" :mask-closable="false" @on-ok="ok" @on-cancel="cancel" width="620">
       <div class="searchBox">
         <div style="margin-bottom:10px;">将档案迁移至 ：</div>
         <div class="rows">
@@ -182,8 +182,8 @@
       </div>
      
       <div slot="footer">
-        <Button @click="migrate = false" type="primary">确定</Button>
-        <Button @click="migrate = false">取消</Button>
+        <Button @click="migrateFields.migrate = false" type="primary">确定</Button>
+        <Button @click="migrateFields.migrate = false">取消</Button>
       </div>
     </Modal>      
   </div>
@@ -196,16 +196,7 @@ export default {
   name: 'file-lists-top',
   data: function () {
     return {
-      search: false,//搜索弹窗
-      exportExcel: false,//导出弹窗
-      exportExcelSuccess: false,//导出成功弹窗
-      
-      migrate: false,//迁移
-      searchBoxTitle: "搜索",
-      exportBoxTitle: "批量导出",
-      exportBoxSuccessTitle: "导出成功",
-      
-      migrateTitle: "迁移档案",
+
       fileAttri: "1",//档案归属 select 默认全部
       fileAttribution: [
         {value: "1",label: "腾邦集团"},
@@ -240,25 +231,20 @@ export default {
       ],
       effectiveDate: "",//有效期时间  
       enteringDate: "", //收文时间
-      info: {
-        contractName: "",//合同名称
+      //搜索需要的字段 ---(最后一个借阅情况没有)
+      searchFields: {
+        search: false,//搜索弹窗
+        searchBoxTitle: "搜索",
         signedSubject: "",//腾邦签署主体
+        groupCompany: "TT",//档案归属  
         businessBrief: "",//业务内容摘要
-        groupCompany: "TT",//档案归属      
-        oppositeCompany: "",//对方公司名称
-        effectiveStartDate: "",//有效期开始时间
-        effectiveEndDate: "",//有效期结束时间
         contractType: "20",//合同类型
         contractNo: "",//合同编号
-        signedUser: "",//签署人
-        enteringType: ["10"],//收文类别
+        effectiveStartDate: "",//有效期开始时间
+        effectiveEndDate: "",//有效期结束时间    
+        enteringDate: "",//收文时间    
         status: "10",//状态
-        enteringDate: "",//收文时间
-        enteringUser: "某某某",//收文人
-      },
-      //搜索需要的字段
-      searchFields: {
-
+        oppositeCompany: "",//对方公司名称
       },
       //批量导入需要的字段
       batchImportFields: {
@@ -266,8 +252,17 @@ export default {
         importBatch: false,//批量导入弹窗
         importBatchTitle: "批量导入",
       },
+      //导出弹窗用到的变量字段
+      exportsExcel: {
+        exportExcel: false,//导出弹窗
+        exportExcelSuccess: false,//导出成功弹窗
+        exportBoxTitle: "批量导出",
+        exportBoxSuccessTitle: "导出成功",
+      },
       //迁移弹窗需要的字段
       migrateFields: {
+        migrate: false,//迁移
+        migrateTitle: "迁移档案",        
         fileRoom: "0",//档案室
         //档案室数据
         fileRoomsData: [
@@ -297,16 +292,16 @@ export default {
     },
     //重置搜索弹窗字段
     reset () {
-      this.info.signedSubject = '';
-      this.info.groupCompany = '1';
-      this.info.businessBrief = '';
-      this.info.contractNo = '';
-      this.info.oppositeCompany = '';
-      this.info.effectiveDate = '';
+      this.searchFields.signedSubject = '';
+      this.searchFields.groupCompany = '1';
+      this.searchFields.businessBrief = '';
+      this.searchFields.contractNo = '';
+      this.searchFields.oppositeCompany = '';
+      this.searchFields.effectiveDate = '';
       this.enteringDate = '';
       this.situation = '0';
-      this.info.status = '0';
-      this.info.contractType = '0';
+      this.searchFields.status = '0';
+      this.searchFields.contractType = '0';
     },
     //手动上传
     handleUpload (file) {
